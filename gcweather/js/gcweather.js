2ce99a7420b2cb50feb04c96568bff39f316554f
Dave Samojlenko <dave.samojlenko@pwgsc.gc.ca>
2013-01-11 13:03:22 -0500

    added custom slideshow code

--- gcweather/js/gcweather.js
+++ gcweather/js/gcweather.js
@@ -27,17 +27,46 @@ function setSelected() {
 	var gcweather_selected = readCookie("gcweather_province_code") + "," + readCookie("gcweather_city_code");
 	jQuery("#gcweather_select").val(gcweather_selected);
 }
-/*
+
 jQuery(document).ready(function(){
 	
+	jQuery("#gcweather-widget ul li").hide();
+	jQuery("#gcweather-widget ul li:first").show().addClass('active');
+	jQuery("#gcweather-widget").prepend('<a id="prev" href="#">Previous</a>');
+	jQuery("#gcweather-widget").append('<a id="next" href="#">Next</a>');
+	
+	jQuery("#next").click(function(e) {
+		e.preventDefault();
+		var $next = jQuery('#gcweather-widget .active').next().length > 0 ? jQuery('#gcweather-widget .active').next() : jQuery("#gcweather-widget ul li").first();
+		jQuery('#gcweather-widget .active').hide("slide", {direction: "left"}, function() {
+			jQuery('#gcweather-widget .active').removeClass('active');
+			$next.addClass('active');
+			$next.show("slide", {direction: "right"});
+		});
+		
+	});
+	
+	jQuery("#prev").click(function(e){
+		e.preventDefault();
+		var $prev = jQuery('#gcweather-widget .active').prev().length > 0 ? jQuery('#gcweather-widget .active').prev() : jQuery("#gcweather-widget ul li").last();
+		jQuery('#gcweather-widget .active').hide("slide", {direction: "left"}, function() {
+			jQuery('#gcweather-widget .active').removeClass('active');
+			$prev.addClass('active');
+			$prev.show("slide", {direction: "right"});
+		});
+		
+	});
+	
+	/*
       Drupal.jsAC.prototype.select = function (node) {
         this.input.value = jQuery(node).data('autocompleteValue');
         if(jQuery(this.input).hasClass('auto_submit')){
           this.input.form.submit();
         }
       };
-    });
-*/
+      */
+});
+
 /*
 jQuery(function() {
 	
