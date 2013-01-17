bb2ebaff83ba529d0f8a3c673ac869b73e4b145d
stephanesylvain <stephane.s.moreau@ssc-spc.gc.ca>
2013-01-17 16:17:16 -0500

    Mega Changes

--- gcweather/js/gcweather.js
+++ gcweather/js/gcweather.js
@@ -28,43 +28,100 @@ function setSelected() {
 	jQuery("#gcweather_select").val(gcweather_selected);
 }
 
-jQuery(document).ready(function(){
+function gcweather_init() {
+	// console.log("huzzah");
+	
 	
 	jQuery("#gcweather-widget ul li").hide();
 	jQuery("#gcweather-widget ul li:first").show().addClass('active');
-	jQuery("#gcweather-widget").prepend('<a id="prev" href="#">Previous</a>');
-	jQuery("#gcweather-widget").append('<a id="next" href="#">Next</a>');
+	jQuery("#gcweather-widget").append('<div class="navigation"><span class="prev"><a href="#">Previous</a></span><span class="next"><a href="#">Next</a></span></div>');
+	//jQuery("#gcweather-widget").append('<a id="next" href="#">Next</a>');
+	jQuery("#gcweather-widget .prev").hide();
+	jQuery("#gcweather-form").hide();
 	
-	jQuery("#next").click(function(e) {
+	jQuery("#gcweather-widget .next").click(function(e) {
 		e.preventDefault();
-		var $next = jQuery('#gcweather-widget .active').next().length > 0 ? jQuery('#gcweather-widget .active').next() : jQuery("#gcweather-widget ul li").first();
+		if (jQuery('#gcweather-widget .active').next().length > 0) {
+			var $next = jQuery('#gcweather-widget .active').next();
+			jQuery("#gcweather-widget .prev").show();
+			$hasPrevButton = true;
+		} /*else {
+			var $next = jQuery("#gcweather-widget ul li").first();
+			jQuery("#prev").hide();
+		}*/
+		
 		jQuery('#gcweather-widget .active').hide("slide", {direction: "left"}, function() {
 			jQuery('#gcweather-widget .active').removeClass('active');
 			$next.addClass('active');
 			$next.show("slide", {direction: "right"});
+			if (jQuery("#gcweather-widget ul li").index(jQuery('#gcweather-widget .active')) == jQuery("#gcweather-widget ul li").length - 1) {
+				jQuery("#gcweather-widget .next").hide();
+				$hasNextButton = false;
+			}
 		});
 		
 	});
 	
-	jQuery("#prev").click(function(e){
+	jQuery("#gcweather-widget .prev").click(function(e){
 		e.preventDefault();
-		var $prev = jQuery('#gcweather-widget .active').prev().length > 0 ? jQuery('#gcweather-widget .active').prev() : jQuery("#gcweather-widget ul li").last();
-		jQuery('#gcweather-widget .active').hide("slide", {direction: "left"}, function() {
+		if (jQuery('#gcweather-widget .active').prev().length > 0) {
+			var $prev = jQuery('#gcweather-widget .active').prev();
+			jQuery("#gcweather-widget .next").show();
+			$hasNextButton = true;
+		} /*else {
+			var $prev = jQuery("#gcweather-widget ul li").last();
+			jQuery("#next").hide();
+		}*/
+		
+		jQuery('#gcweather-widget .active').hide("slide", {direction: "right"}, function() {
 			jQuery('#gcweather-widget .active').removeClass('active');
 			$prev.addClass('active');
-			$prev.show("slide", {direction: "right"});
+			$prev.show("slide", {direction: "left"});
+			if (jQuery("#gcweather-widget ul li").index(jQuery('#gcweather-widget .active')) == 0) {
+				jQuery("#gcweather-widget .prev").hide();
+				$hasPrevButton = false;
+			}
 		});
 		
 	});
 	
-	/*
-      Drupal.jsAC.prototype.select = function (node) {
-        this.input.value = jQuery(node).data('autocompleteValue');
-        if(jQuery(this.input).hasClass('auto_submit')){
-          this.input.form.submit();
-        }
-      };
-      */
+	jQuery("#gcweather-widget .settings").click( function (e) { 
+		e.preventDefault();		
+		jQuery("#gcweather-form").show();
+		jQuery("#gcweather-form #edit-select-province").focus();		
+	});
+	
+	jQuery("#gcweather-form").submit( function () { 
+		jQuery("#gcweather-form").hide("slide", {direction: "up"});
+	});
+	
+}
+
+function getParameterByName(data,name)
+{
+  name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
+  var regexS = "[\\?&]" + name + "=([^&#]*)";
+  var regex = new RegExp(regexS);
+  var results = regex.exec(data);
+  if(results == null)
+    return "";
+  else
+    return decodeURIComponent(results[1].replace(/\+/g, " "));
+}
+
+jQuery(document).ajaxComplete(function(evt,xhr,settings){
+	console.log(evt);
+	console.log(xhr);
+	console.log(settings);
+	console.log(getParameterByName(settings.data,"_triggering_element_name"));
+	var triggering_element = getParameterByName(settings.data,"_triggering_element_name");
+	if(triggering_element != 'select_province') {
+		gcweather_init();
+	}
+});
+
+jQuery(document).on("ready", function(){
+	gcweather_init();
 });
 
 /*
@@ -85,4 +142,3 @@ jQuery(function() {
 	
 });
 */
-
